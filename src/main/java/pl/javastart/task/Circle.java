ae6b8f0f9e5b759c66354f01ff03a2055e85b2b3
Marcin Kunert <the.martines@gmail.com>
2022-05-05 12:57:52 +0200

    Rozwiązanie zad. 4.3.

---
+++ src/main/java/pl/javastart/task/Circle.java
@@ -0,0 +1,18 @@
+package pl.javastart.task;
+
+public class Circle {
+    private int r;
+
+    public Circle(int r) {
+        this.r = r;
+
+    }
+
+    public int getR() {
+        return r;
+    }
+
+    public void setR(int r) {
+        this.r = r;
+    }
+}
--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,8 +1,23 @@
 package pl.javastart.task;
 
 public class Main {
-
     public static void main(String[] args) {
 
+        Square square = new Square(5);
+        ShapeCalculator calc = new ShapeCalculator();
+        double squareArea = calc.calculateSquareArea(square);
+        System.out.println("pole kwadratu wynosi : " + squareArea);
+
+        Circle circle = new Circle(3);
+        double circleArea = calc.calculateCircleArea(circle);
+        System.out.println("Pole koła wynosi : " + circleArea);
+
+        Triangle triangle = new Triangle(1, 2, 3);
+        double trianglePerimeter = calc.calculateTrianglePerimeter(triangle);
+        System.out.println("Obwód trójkąta wynosi : " + trianglePerimeter);
+
+        Rectangle rectangle = new Rectangle(1, 2);
+        double rectPerimeter = calc.calculateRectPerimeter(rectangle);
+        System.out.println("Obwód prostokąta wynosi :  " + rectPerimeter);
     }
 }
